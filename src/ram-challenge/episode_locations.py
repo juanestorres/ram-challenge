e8a5a70fffbb6a67ddd51cc0388df926be84a51a
Juan Esteban Torres <juanes_1323@hotmail.com>
2022-01-04 10:38:53 -0500

    Documentation added

--- src/ram-challenge/episode_locations.py
+++ src/ram-challenge/episode_locations.py
@@ -2,30 +2,37 @@ from datetime import datetime
 from datetime import timedelta
 
 def episode_locations(characters_json, episodes_json, start):
+
     #Final answer json(dictionary)
     second_answer = {
         "exercise_name": "Episode locations",
         "time": "",
         "in_time": False,
     }
+
+    #Extract the episodes and characters (ordered) list and create an empty list to append the results
     episodes_list=episodes_json["results"]
     characters_list = characters_json["results"]
     results = []
+
+
     for episode in episodes_list:
+        #Set a new array of locations and its size. Besides, getting the chracters that appeared in that episode
         locations= []
         locations_size = 0
         characters_in_episode = episode["characters"]
-        #print("---------")
+        
         for character in characters_in_episode:
+            #Get the character id and access its information from the list (which is ordered).
             character_id = int(character.split('/')[-1]) - 1
-            #if character_id > len(characters_list) - 1:
-            #    break
-            #print(character_id + 1)
             character_data = characters_list[character_id]
             character_origin = character_data["origin"]["name"]
+            #Check if that location is not already in the list. if so then add it.
             if character_origin not in locations:
                 locations.append(character_origin)
                 locations_size+= 1
+        
+        #Create the json structure and append the result
         result = {
                 "name": episode["name"],
                 "episode": episode["episode"],
--- src/ram-challenge/ram-challenge-logic.py
+++ src/ram-challenge/ram-challenge-logic.py
@@ -12,20 +12,24 @@ episodes_url = "https://rickandmortyapi.com/api/episode"
 locations_url = 'https://rickandmortyapi.com/api/location'
 
 def fetch_data(url):
-    """Fetches the API data using requests module.
+    """Fetches the API data using requests module in a sequential way.
 
     Args:
         url (string): The API's url where the data comes from.
 
     Returns:
-        [Response]: Returns the response object.
+        [Dict]: Returns the response content in json (dict) format.
     """
+
+    #Get first response
     response = requests.get(url)
     response_json = response.json()
+    #Get the next page url
     next_page_url = response_json["info"]["next"]
 
-
-    while next_page_url:
+    #Check if that url is not empty.
+    while next_page_url: 
+        #Get the new response and append the results to thee first one.
         new_response = requests.get(next_page_url)
         new_response_json = new_response.json()
         response_json['results'] += new_response_json['results']
@@ -34,22 +38,36 @@ def fetch_data(url):
 
 
 def fetch_data_multithread(url:str):
+    """Fetches the API data using requests module in a multithreaded way.
 
+    Args:
+        url (string): The API's url where the data comes from.
+
+    Returns:
+        [Dict]: Returns the response content in json (dict) format.
+    """
+    #Get the first response
     response = requests.get(url)
     response_json = response.json()
+
+    #Check if the response is not empty.
     if not response_json:
         raise "Empty data"
+    #Check if the url is either for episodes, locations or character
     object_type = url.split('/')[-1]
+    #Create the base url based on the type
     base_url = f'https://rickandmortyapi.com/api/{object_type}?page='
+
+    #Create a list with all the urls (for all the pages)
     number_pages = int(response_json['info']['pages'])
     pages_url_list = []
-
     for page in range(2, number_pages + 1):
         new_url = base_url + str(page)
         pages_url_list.append(new_url)
 
     print(str(number_pages) + " ---- " + pages_url_list[-1])
 
+    #Create a Thread Pool for executing all the API's calls and then merge all the results
     threads = []
     with ThreadPoolExecutor(max_workers=20) as executor:
         for page_url in pages_url_list:
@@ -57,19 +75,36 @@ def fetch_data_multithread(url:str):
         for task in as_completed(threads):
             response_json['results'] += task.result()['results']
 
+    #Sort the results since they are no longer ordered
     response_json['results'] = sorted(response_json['results'], key=lambda d: d['id'])
     print("Size: " + str(len(response_json["results"])) + ". " + str(response_json["results"][-1]["id"]))
 
     return response_json
 
 def aux_fetch(url):
+    """Aux function that Fetches the API data using requests module
+
+    Args:
+        url (string): The API's url where the data comes from.
+
+    Returns:
+        Returns the response content in json (dict) format.
+    """
     return requests.get(url).json()
 
 
 
 
 
-def rick_and_morty_solution():
+def rick_and_morty_solution(local):
+    """Main function responsable for getting the API data and delegating each exercise to it's own module.
+
+    Args:
+        local (Bool): Determine if the function will run in a local environment or in production. 
+
+    Returns:
+        [Dict]: Json (dict) with the answers of each exercise.
+    """
     try:
         #Start time measurement for first exercise
         start = datetime.now()
@@ -84,23 +119,30 @@ def rick_and_morty_solution():
         episodes_json = fetch_data_multithread(episodes_url)
         locations_json = fetch_data_multithread(locations_url)
 
+        #Get json answer for char counter exercise.
         char_counter_json = char_counter(characters_json, episodes_json, locations_json, start)
 
         #Start time measurement for second exercise
         start = datetime.now()
 
+        #Get json answer for the episode locations exercise.
         episode_locations_json = episode_locations(characters_json, episodes_json, start)
 
+        #Create Json file
         answers = [char_counter_json, episode_locations_json]
-        with open('results.json', 'w') as file:
-            file.write(json.dumps(answers))
-            file.close()
+        json_file = json.dumps(answers)
+        if local:
+            with open('results.json', 'w') as file:
+                file.write(json_file)
+                file.close()
+        else:
+            return json_file
 
     except Exception as e:
         print("There was an error fetching the information. " + str(traceback.format_exc()))
 
 
 
-rick_and_morty_solution()
+rick_and_morty_solution(local=True)
 
 
